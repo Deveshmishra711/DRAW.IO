8398b94764891cb32e3aa4dc3abbd283255f0c91
Deveshmishra711 <devesh.cs28@iilm.edu>
2025-08-19 12:59:46 +0530

    Update first.java

--- first.java
+++ first.java
@@ -2,7 +2,7 @@ public class First {
     // print your name 01
     // print your address 02
     public static void main(String[] args){
-        System.out.println("arpit ");
+        System.out.println("devesh ");
         System.out.println("noida");
 
 
